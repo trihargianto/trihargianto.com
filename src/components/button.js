52c1400e0e74268ac221f4bcb6489793ec696bc1
Tri Hargianto <trihargianto@gmail.com>
2020-09-26 14:47:03 +0700

    create Button component

---
+++ src/components/button.js
@@ -0,0 +1,19 @@
+import React from "react"
+import styled from "styled-components"
+
+const StyledButton = styled.button`
+  padding: 15px 20px;
+  background-color: #5c7cfa;
+  color: white;
+  border-radius: 4px;
+  border-width: 0;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3b5bdb;
+  }
+`
+
+export default function Button({ children }) {
+  return <StyledButton type="button">{children}</StyledButton>
+}
