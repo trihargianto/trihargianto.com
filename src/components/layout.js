ccb35e64c99c209ea1c7b661e1c4099bd9476ee6
Tri Hargianto <trihargianto@gmail.com>
2020-09-26 14:49:09 +0700

    put Navbar & Gretting component into homepage

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Navbar from "./navbar"
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
@@ -21,14 +23,17 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
+    <div>
+      <Navbar />
+
+      <div className="global-wrapper" data-is-root-path={isRootPath}>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,75 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Gretting from "../components/gretting"
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+const SectionTitle = styled.h3`
+  font-size: 28px;
+  margin: 10px 0px;
+  padding-bottom: 12px;
+  border-bottom: 1px solid #eee;
+`
 
+const BlogPosts = ({ posts }) => {
   if (posts.length === 0) {
     return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
+      <p>
+        No blog posts found. Add markdown posts to "content/blog" (or the
+        directory you specified for the "gatsby-source-filesystem" plugin in
+        gatsby-config.js).
+      </p>
     )
   }
+
+  return posts.map(post => {
+    const title = post.frontmatter.title || post.fields.slug
+    return (
+      <article
+        key={post.fields.slug}
+        className="post-list-item"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header>
+          <h2>
+            <Link to={post.fields.slug} itemProp="url">
+              <span itemProp="headline">{title}</span>
+            </Link>
+          </h2>
+          <small>{post.frontmatter.date}</small>
+        </header>
+        <section>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: post.frontmatter.description || post.excerpt,
+            }}
+            itemProp="description"
+          />
+        </section>
+      </article>
+    )
+  })
+}
+
+const Homepage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
-      <Bio />
-      {posts.map(post => {
-        const title = post.frontmatter.title || post.fields.slug
-        return (
-          <article
-            key={post.fields.slug}
-            className="post-list-item"
-            itemScope
-            itemType="http://schema.org/Article"
-          >
-            <header>
-              <h2>
-                <Link to={post.fields.slug} itemProp="url">
-                  <span itemProp="headline">{title}</span>
-                </Link>
-              </h2>
-              <small>{post.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: post.frontmatter.description || post.excerpt,
-                }}
-                itemProp="description"
-              />
-            </section>
-          </article>
-        )
-      })}
+      <Gretting />
+
+      <SectionTitle>Artikel</SectionTitle>
+      <BlogPosts posts={posts} />
     </Layout>
   )
 }
 
-export default BlogIndex
+export default Homepage
 
 export const pageQuery = graphql`
   query {
