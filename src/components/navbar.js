eecbc5d9f3cd66a16708d537663b202c3edc79be
Tri Hargianto <trihargianto@gmail.com>
2020-09-26 14:48:32 +0700

    create Navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,68 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #eee;
+`
+
+const Logo = styled(Link)`
+  font-size: 18px;
+  font-weight: bold;
+  text-decoration: none;
+`
+
+const Nav = styled.nav`
+  width: 900px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 0;
+`
+
+const ListWrapper = styled.ul`
+  padding: 0;
+  margin: 0;
+`
+
+const List = styled.li`
+  list-style: none;
+  float: left;
+  margin-left: 15px;
+  margin-bottom: 0px;
+`
+
+const ListLink = styled(Link)`
+  padding: 10px 15px;
+  text-decoration: none;
+
+  background-color: #edf2ff;
+  color: #5c7cfa;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #5c7cfa;
+    color: white;
+  }
+`
+
+export default function Navbar() {
+  return (
+    <Wrapper>
+      <Nav>
+        <Logo to="/">💾 &nbsp;Tri Hargianto</Logo>
+
+        <ListWrapper>
+          <List>
+            <ListLink to="/">Blog</ListLink>
+          </List>
+          <List>
+            <ListLink to="/">About</ListLink>
+          </List>
+        </ListWrapper>
+      </Nav>
+    </Wrapper>
+  )
+}
