3a2b9fdd166908592f0bf37c7e7d146b92b36ecd
Tri Hargianto <trihargianto@gmail.com>
2023-12-19 11:24:35 +0700

    add publications

---
+++ content/publications.ts
@@ -0,0 +1,33 @@
+export const publications = [
+  {
+    title:
+      "Deciding the Minimum Browser Versions to be Supported in Mamikos.com",
+    date: "Dec 5, 2022",
+    link: "https://medium.com/mamitech/deciding-the-minimum-browser-versions-to-be-supported-in-mamikos-com-e493d2d04caf",
+  },
+  {
+    title: "Pull Request Principles in Mamikos",
+    date: "May 21, 2022",
+    link: "https://medium.com/mamitech/pull-request-principles-in-mamikos-ab6a7390aeac",
+  },
+  {
+    title: "Stop berusaha nge-hafal syntax bahasa pemrograman ✋",
+    date: "Jun 3, 2020",
+    link: "https://medium.com/jogjajs-community/stop-berusaha-nge-hafal-syntax-bahasa-pemrograman-f56fd8b0ab5b",
+  },
+  {
+    title: "Javascript Fundamental — Scope : Part 3 [Bahasa Indonesia]",
+    date: "May 31, 2020",
+    link: "https://medium.com/jogjajs-community/javascript-fundamental-scope-part-3-bahasa-indonesia-dc5e524e39de",
+  },
+  {
+    title: "Javascript Fundamental — Scope : Part 2 [Bahasa Indonesia]",
+    date: "May 18, 2020",
+    link: "https://medium.com/jogjajs-community/javascript-fundamental-scope-part-2-bahasa-indonesia-ca810a701260",
+  },
+  {
+    title: "Javascript Fundamental — Scope : Part 1 [Bahasa Indonesia]",
+    date: "May 16, 2020",
+    link: "https://medium.com/@trihargianto/javascript-fundamental-scope-part-1-bahasa-indonesia-af160ec5a52d",
+  },
+];
--- src/components-v2/03-organisms/LatestArticle/LatestArticle.tsx
+++ src/components-v2/03-organisms/LatestArticle/LatestArticle.tsx
@@ -4,18 +4,24 @@ import { Link } from "gatsby";
 import Button from "../../01-atoms/Button";
 
 interface LatestArticleTypes {
+  sectionTitle: string;
   articles: {
     title: string;
     date: string;
-    slug: string;
+    slug?: string;
+    link?: string;
   }[];
   className: string;
 }
 
-const LatestArticle = ({ articles, className }: LatestArticleTypes) => (
+const LatestArticle = ({
+  sectionTitle,
+  articles,
+  className,
+}: LatestArticleTypes) => (
   <div className={className}>
     <div className="mb-8 flex items-center justify-between">
-      <h2 className="text-2xl font-semibold sm:text-3xl">Artikel Terbaru</h2>
+      <h2 className="text-2xl font-semibold sm:text-3xl">{sectionTitle}</h2>
       <Button>Lihat Semua</Button>
     </div>
 
@@ -24,9 +30,20 @@ const LatestArticle = ({ articles, className }: LatestArticleTypes) => (
         key={`latest-article-${index}`}
         className="flex flex-row justify-between border-b-2 py-3"
       >
-        <Link to={item.slug} className="font-medium hover:underline">
-          {item.title}
-        </Link>
+        {item.slug ? (
+          <Link to={item.slug} className="font-medium hover:underline">
+            {item.title}
+          </Link>
+        ) : (
+          <a
+            href={item.link}
+            className="font-medium hover:underline"
+            target="__blank"
+            rel="noopener noreferrer"
+          >
+            {item.title}
+          </a>
+        )}
 
         <span className="hidden text-slate-400 md:block">{item.date}</span>
       </div>
