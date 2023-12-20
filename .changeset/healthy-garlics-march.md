7e90b8e7b86d2624f927a233fb4fc51890b8c702
Tri Hargianto <trihargianto@gmail.com>
2023-12-20 23:04:37 +0700

    feat(app): add Inter font

---
+++ .changeset/healthy-garlics-march.md
@@ -0,0 +1,5 @@
+---
+"tri-hargianto-official-website": minor
+---
+
+Add Inter font
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -129,6 +129,24 @@ const config: GatsbyConfig = {
       },
       __key: "content",
     },
+
+    // https://github.com/codeAdrian/gatsby-omni-font-loader
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Inter`,
+            file: `https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap`,
+          },
+        ],
+      },
+    },
   ],
 };
 
