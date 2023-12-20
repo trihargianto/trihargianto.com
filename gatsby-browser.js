67694c42e87dc5ce1226bce710a4402af6e8958f
Tri Hargianto <trihargianto@gmail.com>
2023-12-20 14:45:31 +0700

    remove unused packages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,5 @@
 import "./src/styles/global.scss";
 
-// custom typefaces
-// import "typeface-montserrat";
-// import "typeface-merriweather";
-
-// custom CSS styles
-// import "./src/style.scss";
-
 // Highlighting for code blocks
 import "prismjs/themes/prism-solarizedlight.css";
 import "prismjs/plugins/command-line/prism-command-line.css";
