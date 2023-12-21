603c826b5e173e978e2a554de750afebdce3e6c4
Tri Hargianto <trihargianto@gmail.com>
2023-12-22 03:24:03 +0700

    feat(app): add favicon

--- src/components/01-atoms/BrandLogo/BrandLogo.tsx
+++ src/components/01-atoms/BrandLogo/BrandLogo.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import clsx from "clsx";
 
+import IconPng from "../../../img/icon.png";
+
 type BrandLogoTypes = {
   className?: string;
 };
 
 const BrandLogo = ({ className = "", ...restProps }: BrandLogoTypes) => (
   <span className={clsx(["text-xl font-semibold", className])} {...restProps}>
-    <span className="mr-1 inline-block">💾</span>{" "}
+    <img
+      src={IconPng}
+      alt="icon"
+      className="mr-1 inline-block"
+      width={28}
+      height={28}
+    />
     <span className="hidden sm:inline-block">Tri Hargianto</span>
   </span>
 );
