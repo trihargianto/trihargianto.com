6c039c382b030d3df6dda1374b4442caa268d21e
Tri Hargianto <trihargianto@gmail.com>
2023-12-25 13:45:26 +0700

    fix(app): article description color

--- src/components/04-templates/BlogPostTemplate/BlogPostTemplate.tsx
+++ src/components/04-templates/BlogPostTemplate/BlogPostTemplate.tsx
@@ -99,7 +99,7 @@ const BlogPostTemplate = ({ data, location }: BlogPostTemplateProps) => {
       <div className="container mx-auto">
         <h1 className="mb-0">{title}</h1>
 
-        <p className="mb-6 mt-1 text-sm text-gray-700 sm:text-base">
+        <p className="mb-6 mt-1 text-sm text-gray-700 dark:text-gray-500 sm:text-base">
           {description}
         </p>
 
