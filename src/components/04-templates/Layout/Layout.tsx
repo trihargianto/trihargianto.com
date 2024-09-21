6b19b61097c905e3eb332997a9007fe126bf9dfa
Tri Hargianto <trihargianto@gmail.com>
2024-09-22 00:27:22 +0700

    remove motion to improve page load time

--- src/components/04-templates/Layout/Layout.tsx
+++ src/components/04-templates/Layout/Layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { motion } from "framer-motion";
 
 import NavigationBar from "../../03-organisms/NavigationBar";
 import Footer from "../../03-organisms/Footer";
@@ -16,13 +15,7 @@ const Layout = ({ children }: LayoutProps) => (
       {/* Must be equal to NavigationBar height */}
       <div style={{ height: 75 }} />
 
-      <motion.div
-        initial={{ opacity: 0, y: 40 }}
-        whileInView={{ opacity: 1, y: 1 }}
-        transition={{ type: "spring", duration: .5 }}
-      >
-        {children}
-      </motion.div>
+      {children}
     </div>
 
     <Footer />
