306d1d815b563b191efe27aa12479e2c871c8a3d
Tri Hargianto <trihargianto@gmail.com>
2025-02-16 07:40:12 +0700

    feat: update Badge color

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,17 +1,44 @@
 import clsx from "clsx";
 
-export default function Badge({ children, isActive = false }) {
-  return (
-    <span
-      className={clsx(
-        "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs",
-        "border-1",
-        isActive
-          ? "bg-blue-500 text-white dark:bg-blue-500 dark:text-white/90 border-transparent"
-          : "bg-gray-50 border-gray-200 dark:border-transparent text-gray-500 dark:bg-white/10 dark:text-white/60",
-      )}
-    >
-      {children}
-    </span>
+type BadgeProps = {
+  children: React.ReactNode;
+  isActive?: boolean;
+  isClickable?: boolean;
+  onClick?: () => void;
+  className?: string;
+  dataTagName?: string;
+};
+
+export default function Badge({
+  children,
+  isActive = false,
+  isClickable = false,
+  className = "",
+  ...props
+}: BadgeProps) {
+  const badgeClasses = clsx(
+    "inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs",
+    "border-1",
+    isActive
+      ? "bg-gray-200 text-black dark:bg-slate-800 dark:text-white border-transparent"
+      : "bg-gray-50 border-gray-200 dark:border-transparent text-gray-500 dark:bg-white/10 dark:text-white/60",
   );
+
+  if (isClickable) {
+    return (
+      <button
+        className={clsx([
+          badgeClasses,
+          className,
+          "cursor-pointer",
+          "hover:bg-gray-200 hover:text-black hover:dark:bg-slate-800 hover:dark:text-white border-transparent",
+        ])}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  return <span className={badgeClasses}>{children}</span>;
 }
