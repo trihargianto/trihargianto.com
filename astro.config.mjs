25ce4adb049b00e6d5f8767ca567db794689e972
Tri Hargianto <trihargianto@gmail.com>
2025-03-03 22:01:05 +0700

    feat: integrate with Sentry (#105)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,49 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import netlify from "@astrojs/netlify";
-
 import expressiveCode from "astro-expressive-code";
+import sentry from "@sentry/astro";
+
+const { SENTRY_DSN, SENTRY_AUTH_TOKEN } = loadEnv(
+  process.env.NODE_ENV || "development",
+  process.cwd(),
+  "",
+);
+
+const sentryDsn = SENTRY_DSN;
+const sentryAuthToken = SENTRY_AUTH_TOKEN;
+
+const integrations = [
+  expressiveCode({
+    themes: ["catppuccin-frappe", "one-light"],
+  }),
+  mdx(),
+  sitemap(),
+  react(),
+];
+
+if (sentryDsn && sentryAuthToken) {
+  integrations.push(
+    sentry({
+      dsn: sentryDsn,
+      sourceMapsUploadOptions: {
+        project: "trihargiantocom",
+        authToken: sentryAuthToken,
+      },
+    }),
+  );
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://trihargianto.com",
-  integrations: [
-    expressiveCode({
-      themes: ["catppuccin-frappe", "one-light"],
-    }),
-    mdx(),
-    sitemap(),
-    react(),
-  ],
+  integrations,
 
   vite: {
     plugins: [tailwindcss()],
@@ -29,3 +51,4 @@ export default defineConfig({
 
   adapter: netlify(),
 });
+
